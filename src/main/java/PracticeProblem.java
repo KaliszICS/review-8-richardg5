6bd4da29226f6b5078656ead73b2d063a21080e7
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-18 19:38:59 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,32 @@ public class PracticeProblem {
 
 	}
 
+	public static String pluralize(String in) {
+		if (in.toLowerCase().endsWith("ey")) {
+			return "eys";
+		} else if (in.toLowerCase().endsWith("y")) {
+			return "ies";
+		} else if (in.toLowerCase().endsWith("ife")) {
+			return "ives";
+		}
+		return "s";
+	}
+
+	public static int min(int in1, int in2, int in3) {
+		int currMin = Math.min(in1, in2);
+		return Math.min(currMin, in3);
+	}
+
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					return true;
+				}
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
 }
